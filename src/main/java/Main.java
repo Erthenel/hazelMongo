bfbc48bf2ebc95090731a5ac5049f14868fbfe39
Evgeny Yakovlev <eugenious-yakovlev@yandex.ru>
2017-05-11 12:49:15 +0300

    Fix output in main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ApplicationContext ctx = new GenericXmlApplicationContext("beans.xml");
         Map map= (Map)ctx.getBean("usermap");
         User usr = new User("Enes", 29);
-        map.put("id-132", usr);
+        map.put("id-134", usr);
         User usr2 = (User) map.get("id-134");
         System.out.println(usr2.getName());
     }
