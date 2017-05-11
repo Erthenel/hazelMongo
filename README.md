4fe46e7275926e6c1d38a766828178cf2941da58
Evgeny Yakovlev <eugenious-yakovlev@yandex.ru>
2017-05-11 12:42:45 +0300

    Add initial data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hazelMongo
 Example from https://blog.hazelcast.com/hazelcast-mongodb/
+
+MongoDB shell version: 2.0.2
+connecting to: test
+> db.user.find()
+{ “_id” : “id-134”, “_class” : “com.hazelmongo.User”, “name” : “Enes”, “age” : 29 }
+>
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Map;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new GenericXmlApplicationContext("beans.xml");
+        Map map= (Map)ctx.getBean("usermap");
+        User usr = new User("Enes", 29);
+        map.put("id-132", usr);
+        User usr2 = (User) map.get("id-134");
+        System.out.println(usr2.getName());
+    }
+}
